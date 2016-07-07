58f35958d2b66007d2b8ea013049a0e85c0c4e8d
coderYSW <15396990889@163.com>
2016-07-07 10:08:05 +0800

    'seconds'

--- app.js
+++ app.js
@@ -1,8 +1,16 @@
+var express = require('express');
+var app = express();
+var path = require('path');
+var logger = require('morgan');
+var port = process.env.PORT || 3000;
 
-var static = require('node-static');
-var http = require('http');
-var file = new(static.Server)();
-var app = http.createServer(function (req, res) {
-  file.serve(req, res);
-}).listen(3000);
+app.use(logger('dev'));
+app.use(express.static(path.join(__dirname,'/static')));
+app.use(function(req,res){
+  res.sendFile(path.join(__dirname,'./static/index.html'));
+});
+
+var server = app.listen(port,function(){
+  console.log('webrtc is runing on port '+ port+'!');
+});
 
